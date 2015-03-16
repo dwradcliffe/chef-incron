a5523ae40e036d2cbd134a15be5d7cf80e81ce49
David Radcliffe <radcliffe.david@gmail.com>
2015-03-16 18:15:24 -0400

    bump version to 0.3.2

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'David Radcliffe'
 maintainer_email 'radcliffe.david@gmail.com'
 license          'MIT'
 description      'Installs and configures incron'
-version          '0.3.1'
+version          '0.3.2'
 
 recipe 'incron',  'Install incron package and starts the service'
 
