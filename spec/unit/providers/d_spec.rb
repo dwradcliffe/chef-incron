1ec21c83cbdefa7d8ec33ac0a127d5a00e7d2dfb
David Radcliffe <radcliffe.david@gmail.com>
2015-04-23 15:38:29 -0400

    use new ChefSpec::SoloRunner

--- spec/unit/providers/d_spec.rb
+++ spec/unit/providers/d_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'incron_d' do
 
-  let(:chef_run) { ChefSpec::Runner.new(step_into: ['incron_d']).converge('incron_spec::simple') }
+  let(:chef_run) { ChefSpec::SoloRunner.new(step_into: ['incron_d']).converge('incron_spec::simple') }
 
   it 'should render fragment' do
     expect(chef_run).to render_file('/etc/incron.d/notify_home_changes').with_content('/home IN_MODIFY /usr/local/bin/abcd')
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 describe 'incron::default' do
-  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+  let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
 
   it 'installs the package' do
     expect(chef_run).to install_package('incron')
   end
 
   context 'in a redhat-based platform' do
-    let(:chef_run) { ChefSpec::Runner.new(:platform => 'redhat', :version  => '6.3').converge(described_recipe) }
+    let(:chef_run) { ChefSpec::SoloRunner.new(:platform => 'redhat', :version  => '6.3').converge(described_recipe) }
 
     before do
       stub_command(/rpm -qa | grep -q '^rpmforge-release-[0-9\.-]'/).and_return(true)
